1351123505bcb2645f7c75b43ba8af639c598dd0
Svetlana Vorokhobina <svetlana.vorokhobina@gmail.com>
2023-12-19 11:36:56 +1300

    feat: add solutions for carbon-dating, extended-repeater, and get-email-domain tasks

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,29 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  let addition;
+  let separator = options.separator || '+';
+  let additionSeparator = options.additionSeparator || '|';
+  
+  function createString(str, num, separ) { //String, Number of repetitions, Separater
+    if (str === undefined) {
+      return '';
+    } else if (str !== undefined && typeof str !== 'string') {
+      str = String(str);
+    } 
+    
+    if (num === undefined) { num = 1; }
+    
+    let arr = [];
+    for (let i = 0; i < num; i++) {
+      arr[i] = str;
+    }
+    return arr.join(separ);
+  }
+  
+  addition = createString(options.addition, options.additionRepeatTimes, additionSeparator);
+  return createString(str + addition, options.repeatTimes, separator);
 }
 
 module.exports = {
