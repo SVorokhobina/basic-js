662dd430809e137e81c818c5afb2a576ed462088
Svetlana Vorokhobina <svetlana.vorokhobina@gmail.com>
2023-12-26 02:30:33 +1300

    feat: add solutions for tasks what-season.js, hanoi-tower.js, common-character-count.js, sort-by-height.js

--- src/sort-by-height.js
+++ src/sort-by-height.js
@@ -11,9 +11,24 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
-function sortByHeight(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function sortByHeight(arr) {
+  let minusOneIndexes = [];
+  let ordinaryNums = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === -1) {
+      minusOneIndexes.push(i);
+    } else {
+      ordinaryNums.push(arr[i]);
+    }
+  }
+  
+  ordinaryNums.sort( (a, b) => a - b );
+  for (let j = 0; j < minusOneIndexes.length; j++) {
+    ordinaryNums.splice(minusOneIndexes[j], 0, -1);
+  }
+
+  return ordinaryNums;
 }
 
 module.exports = {
